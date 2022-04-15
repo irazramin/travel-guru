cb8086a4cfe74f5a62e712fe4586f7e3c7102a19
irazramin <irazramin@gmail.com>
2022-04-15 16:32:35 +0600

    header section added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
+import News from "./components/News/News";
 import Registration from "./components/Registration/Registration";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/home" element={<Home />}></Route>
+          <Route path="/news" element={<News />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/login" element={<Registration />}></Route>
         </Routes>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../img/logo.png';
+import './Header.css';
 const Header = () => {
+    const navs = [
+      { id: 1, link: 'home', path: '/home' },
+      { id: 2, link: 'News', path: '/news' },
+      { id: 3, link: 'Destination', path: '/destination' },
+      { id: 4, link: 'Blog', path: '/blog' },
+      { id: 1, link: 'Contact', path: '/contact' },
+    ];
   return (
     <header>
-        <nav>
-            
-        </nav>
+      <nav className='navbar'>
+        <div className='nav-logo'>
+          <img src={logo} alt='' />
+        </div>
+        <div>
+          <input
+            type='text'
+            className='nav-search'
+            placeholder='Search your Destination...'
+          />
+        </div>
+        <div className='nav-items'>
+          <ul>
+            {navs.map((item) => {
+              return (
+                <li>
+                  <Link to={item.path}>{item.link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default Header
